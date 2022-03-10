ce5776823274ad6d1f9b13007517d75fec9179e9
Marina <prudyusmarina@gmail.com>
2022-03-10 12:19:14 +0200

    fix 1n, 1m

--- lesson_02/1m.php
+++ lesson_02/1m.php
@@ -1,27 +1,29 @@
 <?php
 
-$lang='en';
-$day=rand(1,7);
- $arr = [
-    'ru'=>['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    'en'=>['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+$lang = rand(0, 1) == 0 ? 'en' : 'ru';
+$day = rand(1, 7);
+$arr = [
+    'ru' => ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'en' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 ];
 
-$arr_ru=$arr['ru'];
-$arr_en=$arr['en'];
+print('day:' . $arr[$lang][$day - 1]);
 
-if($lang=='ru'){
-      foreach($arr_ru as $key => $value){
-        if(($key+1)==$day){
-         print ($value);
-        }
-     }
-  } else{
-       foreach($arr_en as $key => $value){
-       if(($key+1)==$day){
-       print ($value);
-      }
-    }
-   }
-
-?>
+//$arr_ru=$arr['ru'];
+//$arr_en=$arr['en'];
+//
+//if($lang=='ru'){
+//      foreach($arr_ru as $key => $value){
+//        if(($key+1)==$day){
+//         print ($value);
+//        }
+//     }
+//  } else{
+//       foreach($arr_en as $key => $value){
+//       if(($key+1)==$day){
+//       print ($value);
+//      }
+//    }
+//   }
+//
+//?>
--- lesson_02/1n.php
+++ lesson_02/1n.php
@@ -1,22 +1,32 @@
 <?php
-$password=12313213111111111111111111111111111111111111;
-print(" пароль". " ". $password."==> ");
- $arr=str_split($password);
-$arr_count=$arr;
+$password = 12313213111111111111111111111111111111111111;
+print(" пароль" . " " . $password . "==> ");
+$pass = str_split($password);
+
+//function first_item (&$arr){
+//   $item=array_shift($arr);
+//   print($item);
+//  }
+
+echo('<br/>');
+print("ваш пароль==>" . " ");
 
 
-function first_item (&$arr){
-   $item=array_shift($arr);
-   print($item);
-  }
+//  for ($i=0;$i<count($arr_count); $i++){
+//    first_item($arr);
+//  }
 
-     echo('<br/>');
-     print("ваш пароль==>"." ");
+function recursion($arr)
+{
+    for ($i = 0; $i < count($arr); $i++) {
+        if (is_array($arr[$i])) {
+            recursion($arr[$i]);
+        } else {
+            echo $arr[$i];
+        }
+    }
+}
+
+recursion($pass);
 
 
-  for ($i=0;$i<count($arr_count); $i++){
-    first_item($arr);
-  }
-
-
-?>
